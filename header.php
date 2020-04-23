88bf6717ad22f713f2e412ac64e2c3b19128f6af
nayeemkhan7 <nayeem.3119@gmail.com>
2020-04-23 21:11:52 +0600

    Index File Spliting

---
+++ header.php
@@ -0,0 +1,26 @@
+<?php
+  ob_start();
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Google Fonts Attach -->
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,500,600,700|Poppins:200,300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <!-- Main CSS Attach -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>NK</title>
+  </head>
+  <body>
+
+
+<!-- =========== Header End =========== -->
